2d1b2849ddd5870c5f7a28bc226d3d3a1f70404f
KidHubz <kidareen@gmail.com>
2025-09-23 09:51:20 -0600

    sub

--- CMPE2400_02_09_2025_Northwind.sql
+++
@@ -1,6 +0,0 @@
-use dkinganjatou1_Northwind
-go
-
-
--- select all the information fro, the products table
-select * from Products
---
+++ My Notes/CMPE2400_02_09_2025_BasicSelect.sql
@@ -0,0 +1,8 @@
+-- Single line comment
+
+/*
+Multiline comment
+*/
+-- use command is used to select any database
+-- use [name od the database]
+

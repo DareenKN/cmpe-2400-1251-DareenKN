e0221e0df179aea99fe825e1fcd775a45e332605
DareenKN <dkinganjatou1@nait.ca>
2025-09-25 10:13:24 -0600

    Create SQLQuery5.sql

---
+++ SQLQuery5.sql
@@ -0,0 +1,24 @@
+use NorthwindTraders
+go
+
+select	ProductID,
+		ProductName,
+		UnitPrice
+from Products
+where Discontinued != 1 and UnitsInStock between 20 and 30
+
+select * from Products
+
+select	CategoryID,
+		CategoryName,
+		"Description"
+from Categories
+where CategoryName like '[SB]%'
+
+select	CustomerID,
+		ContactName,
+		City,
+		Region
+from Customers
+where Fax is null and Region is not null 
+order by Country asc, City desc

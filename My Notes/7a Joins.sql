84dcc3c3caaf3790c872a3e3e9c7e909fb7b6902
DareenKN <dkinganjatou1@nait.ca>
2025-10-31 08:47:33 -0600

    -- Find employees who handled orders of the single costlier product -- HINT: Which employee(s) processed the orders that contained the product -- with the highest unit price

--- My Notes/7a Joins.sql
+++ My Notes/7a Joins.sql
@@ -52,3 +52,74 @@ where p2.CategoryID =   (
                                                     from Products p1
                                                 )
                         )
+
+use dkinganjatou1_Northwind
+go
+-- Find employees who handled orders of the single costlier product
+-- HINT: Which employee(s) processed the orders that contained the product
+-- with the highest unit price
+
+select *
+from products
+
+select *
+from [order Details] od
+
+select *
+from Employees e
+
+-- Find the max unit price
+select p.ProductID
+from Products p
+where p.UnitPrice = (
+                        select max(p1.UnitPrice)
+                        from Products p1
+                    )
+
+-- Find the orderID which have ProductID = 38
+select od.OrderID
+from [order Details] od
+where od.ProductID =    (
+                            select p.ProductID
+                            from Products p
+                            where p.UnitPrice = (
+                                                    select max(p1.UnitPrice)
+                                                    from Products p1
+                                                )
+                        )
+
+-- Find the employeesID related to those orders
+select o.EmployeeID
+from [orders] o
+where o.OrderID in  (
+                        select od.OrderID
+                        from [order Details] od
+                        where od.ProductID =    (
+                                                    select p.ProductID
+                                                    from Products p
+                                                    where p.UnitPrice = (
+                                                                            select max(p1.UnitPrice)
+                                                                            from Products p1
+                                                                        )
+                                                )
+                    )
+
+-- Select all the information for the concerned employees
+select *
+from Employees e
+where e.EmployeeID in   (
+                            select o.EmployeeID
+                            from [orders] o
+                            where o.OrderID in  (
+                                                    select od.OrderID
+                                                    from [order Details] od
+                                                    where od.ProductID =    (
+                                                                                select p.ProductID
+                                                                                from Products p
+                                                                                where p.UnitPrice = (
+                                                                                                        select max(p1.UnitPrice)
+                                                                                                        from Products p1
+                                                                                                    )
+                                                                            )
+                                                )
+                        )

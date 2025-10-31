3852a0e2e6c9ed1358c300ae62f0d1a9649c5cf5
DareenKN <dkinganjatou1@nait.ca>
2025-10-31 09:33:01 -0600

    Cover Inner joins

--- My Notes/7a Joins.sql
+++ My Notes/7a Joins.sql
@@ -4,3 +4,36 @@
 -- JOINS
 -- INNER JOINS
 -- OUTER JOIN[LEFT, RIGHT]
+-- CROSS JOIN
+
+/*
+    A join in SQL is used to combine data from to or more tables 
+    based on a RELATED column between them.
+    PK, FK -- Usually it is PK and FK columns,
+    but it could be other columns as well as provided these are of the same
+    data type and same values
+*/
+
+use dkinganjatou1_Northwind
+go
+
+-- INNER join
+-- ANSI Standard
+select * from Products
+select * from Categories
+
+-- inner join
+select *
+from Products p
+    inner join Categories c -- Keyword inner is optional
+    on p.CategoryID = c.CategoryID -- Joining condition
+
+-- Specific column
+select  p.ProductID,
+        p.ProductName,
+        c.CategoryName
+from Products p
+    inner join Categories c -- Keyword inner is optional
+    on p.CategoryID = c.CategoryID -- Joining condition
+
+-- SQL SERVER

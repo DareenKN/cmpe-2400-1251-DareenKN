85a0bea3e7909efe68479c81b55bda5b4793c4f3
DareenKN <dkinganjatou1@nait.ca>
2025-11-20 15:33:39 -0700

    Created ica 9

---
+++ ICAs/ica9.sql
@@ -0,0 +1,10 @@
+-- Question 1
+/*
+This question assumes use of NorthwindTraders database.
+
+Rearrange the SQL Statement to return a list containing the results of both the following:
+
+Customer Company Names which have no orders
+Supplier Company Names which have no products
+The entire resultset must be ordered by Company Name
+*/
